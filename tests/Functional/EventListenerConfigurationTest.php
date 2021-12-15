8930dfccaae38b4fada2e244bcd09c8c7510726a
Emir Kurtovic <rapttor@gmail.com>
2021-12-15 14:39:03 +0100

    Initial commit

---
+++ tests/Functional/EventListenerConfigurationTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional;
+
+use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
+use Yiisoft\Di\Container;
+use Yiisoft\Di\ContainerConfig;
+use Yiisoft\Yii\Event\ListenerConfigurationChecker;
+use Yiisoft\Yii\Runner\ConfigFactory;
+
+use function dirname;
+
+class EventListenerConfigurationTest extends TestCase
+{
+    public function testConsoleListenerConfiguration(): void
+    {
+        $config = ConfigFactory::create(dirname(__DIR__, 2), null);
+
+        $containerConfig = ContainerConfig::create()
+            ->withDefinitions($config->get('console'));
+        $container = (new Container($containerConfig))->get(ContainerInterface::class);
+        $checker = $container->get(ListenerConfigurationChecker::class);
+        $checker->check($config->get('events-console'));
+
+        self::assertInstanceOf(ListenerConfigurationChecker::class, $checker);
+    }
+}
